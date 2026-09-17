1ebf45483646473d2cbd5b6cc5a1c30f902b2255
Ducci <yubi369@gmail.com>
2026-09-17 11:55:29 +0900

    버그야 죽어다오

--- My project (1)/Assets/PlayerController.cs
+++ My project (1)/Assets/PlayerController.cs
@@ -5,20 +5,37 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    public float moveSpeed = 15f;
 
     public float jumpPower = 5f;
 
     public float gravity = -20f;
 
+    public float mouseSensitivity = .2f;
+
+    private Vector2 lookInput;
+
+    private bool isRunning;
+
+    public Transform cameraPivot;
+
+    public Transform cameraTransform;
+
+    private float pitch = 20f;
+
+
+
+
     private float verticalVelocity;
     private Vector2 moveInput;
-    private CharacterController contoller;
+    private CharacterController controller;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        contoller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     public void OnMove(InputValue value)
@@ -28,26 +45,57 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputValue value)
     {
-        if (value.isPressed && contoller.isGrounded)
+        if (value.isPressed && controller.isGrounded)
         {
             verticalVelocity = jumpPower;
         }
     }
 
+    public void OnSprint(InputValue value)
+    {
+        isRunning = value.isPressed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
-        if (contoller.isGrounded && verticalVelocity < 0f)
+        transform.Rotate(0f, lookInput.x * mouseSensitivity, 0f);
+        if (controller.isGrounded && verticalVelocity < 0f)
         {
             verticalVelocity = -2f;
         }
 
+        pitch = pitch - lookInput.y * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, -20f, 60f);
+        cameraPivot.localEulerAngles = new Vector3(pitch, 0f, 0f);
+
         verticalVelocity += gravity * Time.deltaTime;
 
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        move *= moveSpeed;
+        Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
+        float speed = moveSpeed;
+        float targetZ = -3f;
+        if (isRunning)
+        {
+            speed = moveSpeed * 2f;
+            targetZ = -5f;
+        }
+        move = move * speed;
         move.y = verticalVelocity;
 
-        contoller.Move(move * Time.deltaTime);
+
+
+        Vector3 camPos = cameraTransform.localPosition;
+        camPos.z = Mathf.Lerp(camPos.z, targetZ, 5f * Time.deltaTime);
+        cameraTransform.localPosition = camPos;
+
+
+        controller.Move(move * Time.deltaTime);
     }
 }
